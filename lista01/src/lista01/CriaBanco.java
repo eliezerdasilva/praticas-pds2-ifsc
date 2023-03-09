e78848efeaf1ea0411075b52f40fe930ce7f205f
eliezer da silva <frete7132@gmail.com>
2023-03-09 09:02:32 -0300

    execicio 8 da lista 01 de pds

--- lista01/src/lista01/CriaBanco.java
+++ lista01/src/lista01/CriaBanco.java
@@ -1,9 +1,41 @@
 package lista01;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Classe de conexao
+ * 
+ * @author eliez
+ * 
+ */
 public class CriaBanco {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", "root",
+					"Eliezer-2005");
+			
+			String query = "create database praticaPDS;";
+			PreparedStatement stm = conn.prepareStatement(query);
+			stm.executeUpdate();
+
+		
+			
+		
+			JOptionPane.showMessageDialog(null, "Conectado", null, JOptionPane.WARNING_MESSAGE);
+			System.out.println("foi");
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
 
 	}
 
